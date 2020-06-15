3e0e68abe50ce8203232f16e82edf93d8d0d6000
Marta Semrau <mm.semrau@gmail.com>
2020-06-15 23:50:46 +0200

    add other functions

--- js/script.js
+++ js/script.js
@@ -5,32 +5,42 @@
 }); */
 
 const titleClickHandler = function(event){
-  console.log(event);
+  const clickedElement = this;
+  event.preventDefault(); /*prevent from scrolling to another section like Wikipedia*/
+  console.log('Link was clicked');
+
 
   /* remove class 'active' from all article links  */
-  const activeLinks = document.querySelectorAll('.titles a.active');
-  for(let activeLink of activeLinks){
+  const activeLinks = document.querySelectorAll('.titles a .active');
+  for (let activeLink of activeLinks){
   activeLink.classList.remove('active');
 }
 
   /* add class 'active' to the clicked link */
-  const clickedElement = this;
-  console.log('clickedElement:', clickedElement);
   clickedElement.classList.add('active');
-
+  console.log('clickedElement:', clickedElement);
 
 
   /* remove class 'active' from all articles */
   const activeArticles = document.querySelectorAll('.posts article.active');
-  for(let activeArticle of activeArticles){
-    activeLink.classList.remove('active');
+  for (let activeArticle of activeArticles){
+    activeArticle.classList.remove('active');
   }
 
   /* get 'href' attribute from the clicked link */
+  const articleSelector = clickedElement.getAttribute('href');
+  console.log(articleSelector);
 
   /* find the correct article using the selector (value of 'href' attribute) */
+  const targetArticle = document.querySelector('articleSelector');
+  console.log(targetArticle);
 
   /* add class 'active' to the correct article */
+  targetArticle.classList.add('active');
+  /* nie wiem dlaczego wyskakuje mi ten błąd..."Cannot read property 'classList' of null
+    at HTMLAnchorElement.titleClickHandler"*/
+
+
 }
 
 const links = document.querySelectorAll('.titles a');

23c93b54f18054eb5c6b6e7a15053aa4bd3bc086
Marta Semrau <mm.semrau@gmail.com>
2020-06-18 22:46:36 +0200

    add new functions

--- js/script.js
+++ js/script.js
@@ -4,6 +4,8 @@
   console.log('links:', links);
 }); */
 
+'use strict';
+
 const titleClickHandler = function(event){
   const clickedElement = this;
   event.preventDefault(); /*prevent from scrolling to another section like Wikipedia*/
@@ -37,11 +39,49 @@ const titleClickHandler = function(event){
 
   /* add class 'active' to the correct article */
   targetArticle.classList.add('active');
-  /* nie wiem dlaczego wyskakuje mi ten błąd..."Cannot read property 'classList' of null
-    at HTMLAnchorElement.titleClickHandler"*/
+
+  const optArticleSelector = '.post',
+      optTitleSelector = '.post-title',
+      optTitleListSelector = '.titles';
+
+  function generateTitleLinks(){
+
+      /* remove contents of titleList */
+      const titleList = document.querySelector(optTitleListSelector);
+      titleList.innerHTML = ''; /*wyrażenie z Clear Messages do wyczyszczenia zawartości listy linków*/
+      console.log(titleList);
+      /*funkcja, która usuwa listę linków po lewej stronie */
 
 
+      /* for each article */
+      const articles = document.querySelector(optArticleSelector);
+      let html = ";
+      for (let article of articles)
+        article.innerHTML = '';
+        console.log(articles);
+
+      /* get the article id (odczytanie id artykułu) */
+      const articleId = article.getAttribute(id); /*zamiast article mogłabym użyć też optArticleSelector? */
+      console.log(articleId);
+
+      /* find the title element (odczytanie tytułu artykułu) */
+      /* get the title from the title element */
+      const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+      console.log(articleTitle)
+      /* jaką funkcję pełni tutaj .innerHTML?  */
+
+      /* create HTML of the link */
+      const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
+      console.log(linkHTML);
+
+      /* insert link into titleList */
+      html = html + linkHTML;
+
+    }
+  titleList.innerHTML = html;
 }
+
+generateTitleLinks();
 
 const links = document.querySelectorAll('.titles a');
 
